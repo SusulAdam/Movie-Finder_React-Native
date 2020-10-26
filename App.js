14a93addacc70945869ab88e994cca9aef02edd6
Adam Susuł <a.w.susul@gmail.com>
2020-10-26 09:49:45 +0100

    Add touchable functionalty: after the user enters the name of the movie, he can recive a response after clicking on the screen and the keyboard automatically closes

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import AppStyles from './styles/AppStyles'
 import apiKey from "./constans/apiKey"
 
 
-export default class App extends React.Component
-{
+export default class App extends React.Component {
 
-  constructor ( props )
-  {
+  constructor ( props ) {
     super( props );
     this.state = {
       searchValue: "",
@@ -17,40 +15,43 @@ export default class App extends React.Component
     };
   }
 
-  GetResponseFromApi = () =>
-  {
+  getResponseFromApi = () => {
     fetch( apiKey + "&s=" + this.state.searchValue )
       .then( response => response.json() )
-      .then( response =>
-      {
+      .then( response => {
         console.log( response );
       } )
   }
 
 
-  handleChangeSearchText = ( text ) =>
-  {
-    this.setState( prevState =>
-    {
+  handleChangeSearchText = ( text ) => {
+    this.setState( prevState => {
       return ( { ...prevState, searchValue: text } )
     } )
   }
 
-  render ()
-  {
+  combinedTouchableFunctions = () => {
+    this.getResponseFromApi()
+    Keyboard.dismiss()
+  }
+
+  render () {
     return (
-      <View style={ AppStyles.container }>
-        <View style={ AppStyles.header }>
-          <Text style={ AppStyles.title }>ComarMovie</Text>
-          <TextInput
-            style={ AppStyles.searchMovie }
-            placeholder="Enter a name for the movie"
-            value={ this.state.searchValue }
-            onChangeText={ text => this.handleChangeSearchText( text ) }
-            onSubmitEditing={ this.GetResponseFromApi }
-          />
+      <TouchableWithoutFeedback onPress={ this.combinedTouchableFunctions }>
+        <View style={ AppStyles.container }>
+          <View style={ AppStyles.header }>
+            <Text style={ AppStyles.title }>ComarMovie</Text>
+            <TextInput
+              style={ AppStyles.searchMovie }
+              placeholder="Enter a name for the movie"
+              value={ this.state.searchValue }
+              onChangeText={ text => this.handleChangeSearchText( text ) }
+              onSubmitEditing={ this.getResponseFromApi }
+            />
+          </View>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
+
     );
   }
 }
